831ad1428df42aa62ad7c5f5bee5b952bd2e1e79
Synaxis <01xtor@gmail.com>
2018-01-25 04:45:15 -0200

    learning bootstrap

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,12 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'LSAPP')}}</title>
     </head>   
-    <body>      
-      @yield('content')
+    <body>
+        @include('inc.navbar')
+        <div class="container">      
+        @yield('content')
     </body>
 </html>
