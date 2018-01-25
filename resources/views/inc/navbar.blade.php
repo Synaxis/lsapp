e797a609be576eaa8828359777d0867374dfb289
Synaxis <01xtor@gmail.com>
2018-01-25 19:49:03 -0200

    added bootstrap and login (no functions yet)

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,20 +1,16 @@
-<nav id="header" class="navbar navbar-fixed-top">
-        <div id="header-container" class="container navbar-container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a id="brand" class="navbar-brand" href="#">Project name</a>
-            </div>
-            <div id="navbar" class="collapse navbar-collapse">
-                <ul class="nav navbar-nav">
-                    <li class="active"><a href="/public/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                </ul>
-            </div><!-- /.nav-collapse -->
-        </div><!-- /.container -->
-    </nav><!-- /.navbar -->
+<nav class="navbar navbar-inverse">
+    <div class="container">
+        <div class="navbar-header">            
+                {{ config('app.name', 'Laravel') }}
+            </a>
+        </div>
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">           
+            <ul class="nav navbar-nav">
+              <li><a href="/">Home</a></li>
+              <li><a href="/about">About</a></li>
+              <li><a href="/services">Services</a></li>
+              <li><a href="/posts">Blog</a></li>
+            </ul>             
+        </div>
+    </div>
+</nav>
